61fba8406a2be1d655bfaf3ac41347c1dfb2d21d
JayaRose <Jayarose8@gmail.com>
2024-09-05 11:56:49 +1200

    added in song
    I added in song and brainstormed some ideas not much progress

--- music_starter.js
+++ music_starter.js
@@ -6,38 +6,26 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   rectMode(CENTER)
   textSize(24);
   
-   let bar_spacing = height / 10;
-   let bar_height = width / 12;
-   let bar_pos_x = width / 2;
- 
 
-   // vocal bar is red
-   fill(200, 0, 0);
-   rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
-   fill(0);
-   text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
+let ballSize = map(vocal, 0, 100, 10, 50)
+let eyeSize = map(drum, 0, 100, 50, 90)
+let sqaureSize = map(bass, 0, 100, 25, 150)
+
+ ellipse(200, 200, eyeSize, eyeSize)
+ ellipse(500, 200, eyeSize, eyeSize)
+ ellipse(300, 200, eyeSize, eyeSize)
+ ellipse(400, 200, eyeSize, eyeSize)
+
+ rect(200, 300, sqaureSize, sqaureSize)
+ rect(500, 300, sqaureSize, sqaureSize)
+ rect(300, 300, sqaureSize, sqaureSize)
+ rect(400, 300, sqaureSize, sqaureSize)
+
+ ellipse(200, 100, ballSize, ballSize)
+ ellipse(500, 100, ballSize, ballSize)
+ ellipse(300, 100, ballSize, ballSize)
+ ellipse(400, 100, ballSize, ballSize)
+
  
-   // drum bar is green
-   fill(0, 200, 0);
-   rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-   fill(0);
-   text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
  
-   // bass bar is blue
-   fill(50, 50, 240);
-   rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
-   fill(0);
-   text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
- 
-   // other bar is white
-   fill(200, 200, 200);
-   rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
-   fill(0);
-   text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
-   fill(255, 255, 0);
- 
-   // display "words"
-   textAlign(CENTER);
-   textSize(vocal);
-   text(words, width/2, height/3);
 }
